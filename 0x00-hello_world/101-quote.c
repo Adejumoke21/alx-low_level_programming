1a313f3a9d4be943ad122546fca955d73c6db429
Abikoye Adejumoke <adeabikoye@gmail.com>
2022-04-07 14:34:16 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-  2 #include <stdlib.h>
-  3
-  4 /**
-  5  * main - Entry point
-  6  * Return: Always 1 (Success)
-  7  */
-  8 int main(void)
-  9 {
- 10         write(STDOUT_FILENO, "and that piece of art is useful
- 11                         \" - Dora Korpar, 2015-10-19\n", 59);
- 12         return (1);
- 13 }
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
+
